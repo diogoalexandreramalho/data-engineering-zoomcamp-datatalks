04b8e5d5f2b545923ab32f1d39cbfae62aaf1e95
dramalhoo17 <37782704+diogoalexandreramalho@users.noreply.github.com>
2022-10-31 17:24:08 +0000

    Adds W1 Q5 homework exercise

--- week_1_basics_n_setup/homework/homework.sql
+++ week_1_basics_n_setup/homework/homework.sql
@@ -15,4 +15,22 @@ LIMIT 1
 
 
 -- Question 5
--- What was the most popular destination for passengers picked up in central parl on January 14?
+-- What was the most popular destination for passengers picked up in central park on January 14?
+-- Use the pick up time for your calculations.
+SELECT dol."Zone" as do_zone, COUNT(*)
+FROM yellow_taxi_trips
+JOIN zones pul
+ON yellow_taxi_trips."PULocationID" = pul."LocationID"
+JOIN zones dol
+ON yellow_taxi_trips."DOLocationID" = dol."LocationID"
+WHERE pul."Zone" LIKE 'Central Park' and CAST(tpep_pickup_datetime as DATE) = '2021-01-14'
+GROUP BY dol."Zone"
+ORDER BY 2 DESC
+LIMIT 1
+
+
+-- Question 6
+-- What's the pickup-dropoff pair with the largest average price for a ride 
+-- (calculated based on total_amount)? Enter two zone names separated by a slash. 
+-- For example:"Jamaica Bay / Clinton East". If any of the zone names are unknown (missing), 
+-- write "Unknown". For example, "Unknown / Clinton East".
